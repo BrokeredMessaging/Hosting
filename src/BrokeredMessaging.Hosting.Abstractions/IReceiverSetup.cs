e34ab6c447d495b09e6a567af0c85e2c990033a0
Paul Turner <programming.hero@gmail.com>
2018-10-14 12:56:44 +0100

    Add abstractions project

---
+++ src/BrokeredMessaging.Hosting.Abstractions/IReceiverSetup.cs
@@ -0,0 +1,16 @@
+﻿using BrokeredMessaging.Messaging;
+
+namespace BrokeredMessaging.Hosting
+{
+    /// <summary>
+    /// Represents the setup of a message-receiver.
+    /// </summary>
+    public interface IReceiverSetup
+    {
+        /// <summary>
+        /// Configures an <see cref="IReceiverBuilder"/> according to the setup.
+        /// </summary>
+        /// <param name="receiver">The builder to configure.</param>
+        void Configure(IReceiverBuilder receiver);
+    }
+}
